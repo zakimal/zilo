6ff9748be4c423456448430a19ecaf1131102021
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-05-26 06:56:55 +0000

    sections

--- zilo.c
+++ zilo.c
@@ -1,3 +1,5 @@
+/*** includes ***/
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -5,7 +7,11 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** data ***/
+
 struct termios orig_termios; // original terminal state
+
+/*** terminal ***/
 
 void die(const char *s)
 {
@@ -36,6 +42,8 @@ void enableRawMode()
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
         die("tcsetattr");
 }
+
+/*** init ***/
 
 int main()
 {
