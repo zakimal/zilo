5530e3846856607124b868feb3b67b8773132755
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-05-27 12:54:45 +0000

    the Home and End key

--- zilo.c
+++ zilo.c
@@ -21,6 +21,8 @@ enum editorKey
     ARROW_RIGHT,
     ARROW_UP,
     ARROW_DOWN,
+    HOME_KEY,
+    END_KEY,
     PAGE_UP,
     PAGE_DOWN
 };
@@ -101,10 +103,18 @@ int editorReadKey()
                 {
                     switch (seq[1])
                     {
+                    case '1':
+                        return HOME_KEY; // HOME: <esc>[1~
+                    case '4':
+                        return END_KEY; // END: <esc>[4~
                     case '5':
                         return PAGE_UP; // PAGE_UP: <esc>[5~
                     case '6':
                         return PAGE_DOWN; // PAGE_DOWN: <esc>[6~
+                    case '7':
+                        return HOME_KEY; // HOME: <esc>[7~
+                    case '8':
+                        return END_KEY; // END: <esc>[8~
                     }
                 }
             }
@@ -113,14 +123,28 @@ int editorReadKey()
                 switch (seq[1])
                 {
                 case 'A':
-                    return ARROW_UP;
+                    return ARROW_UP; // ARROW_UP: <esc>[A
                 case 'B':
-                    return ARROW_DOWN;
+                    return ARROW_DOWN; // ARROW_DOWN: <esc>[B
                 case 'C':
-                    return ARROW_RIGHT;
+                    return ARROW_RIGHT; // ARROW_RIGHT: <esc>[C
                 case 'D':
-                    return ARROW_LEFT;
+                    return ARROW_LEFT; // ARROW_LEFT: <esc>[D
+                case 'H':
+                    return HOME_KEY; // HOME: <esc>[H
+                case 'F':
+                    return END_KEY; // END: <esc>[F
                 }
+            }
+        }
+        else if (seq[0] == 'O')
+        {
+            switch (seq[1])
+            {
+            case 'H':
+                return HOME_KEY; // HOME: <esc>OH
+            case 'F':
+                return END_KEY; // END: <esc>OF
             }
         }
         return '\x1b';
@@ -317,6 +341,14 @@ void editorProcessKeypress()
         write(STDOUT_FILENO, "\x1b[2J", 4);
         write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
+        break;
+
+    case HOME_KEY:
+        E.cx = 0;
+        break;
+
+    case END_KEY:
+        E.cx = E.screencols - 1;
         break;
 
     case ARROW_UP:
