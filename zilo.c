483d848f59cc995a4b5a0e969761db2903786b86
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-05-27 10:28:04 +0000

    press Ctrl-Q to quit

--- zilo.c
+++ zilo.c
@@ -7,6 +7,10 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k)&0x1f) // bitwise-AND with 00011111
+
 /*** data ***/
 
 struct termios orig_termios; // original terminal state
@@ -73,7 +77,7 @@ int main()
             printf("%d ('%c')\r\n", c, c);
         }
 
-        if (c == 'q')
+        if (c == CTRL_KEY('q'))
             break;
     }
     return 0;
