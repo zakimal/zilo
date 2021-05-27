521b68847c3cbd78299fa3c36ae0ab7208050359
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-05-27 10:56:20 +0000

    clear the screen on exit

--- zilo.c
+++ zilo.c
@@ -19,6 +19,9 @@ struct termios orig_termios; // original terminal state
 
 void die(const char *s)
 {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
@@ -81,6 +84,8 @@ void editorProcessKeypress()
     switch (c)
     {
     case CTRL_KEY('q'):
+        write(STDOUT_FILENO, "\x1b[2J", 4);
+        write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
         break;
     }
