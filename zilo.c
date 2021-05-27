1f4dfb16d80b7163ccdbc1e46b32849f156a0d14
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-05-27 12:03:17 +0000

    welcome message

--- zilo.c
+++ zilo.c
@@ -11,6 +11,8 @@
 
 /*** defines ***/
 
+#define ZILO_VERSION "0.0.1"
+
 #define CTRL_KEY(k) ((k)&0x1f) // bitwise-AND with 00011111
 
 /*** data ***/
@@ -156,7 +158,28 @@ void editorDrawRows(struct abuf *ab)
     int y;
     for (y = 0; y < E.screenrows; y++)
     {
-        abAppend(ab, "~", 1);
+        if (y == E.screenrows / 3)
+        {
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome), "Zilo editor -- version %s", ZILO_VERSION);
+            if (E.screencols < welcomelen)
+                welcomelen = E.screencols;
+
+            // centering welcome message
+            int padding = (E.screencols - welcomelen) / 2;
+            if (padding)
+            {
+                abAppend(ab, "~", 1);
+                padding--;
+            }
+            while (padding--)
+                abAppend(ab, " ", 1);
+            abAppend(ab, welcome, welcomelen);
+        }
+        else
+        {
+            abAppend(ab, "~", 1);
+        }
 
         abAppend(ab, "\x1b[K", 3);
         if (y < E.screenrows - 1)
