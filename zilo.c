103727ff5e9eae36f53c88348715590ea656687b
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-05-28 14:40:16 +0000

    scrolling with page up and page down

--- zilo.c
+++ zilo.c
@@ -524,6 +524,26 @@ void editorProcessKeypress()
         E.cx = E.screencols - 1;
         break;
 
+    case PAGE_UP:
+    case PAGE_DOWN:
+    {
+        if (c == PAGE_UP)
+        {
+            E.cy = E.rowoff;
+        }
+        else if (c == PAGE_DOWN)
+        {
+            E.cy = E.rowoff + E.screenrows - 1;
+            if (E.numrows < E.cy)
+                E.cy = E.numrows;
+        }
+
+        int times = E.screenrows;
+        while (times--)
+            editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+        break;
+    }
+
     case ARROW_UP:
     case ARROW_DOWN:
     case ARROW_LEFT:
