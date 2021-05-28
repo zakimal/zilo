9e8f6252097ee0c22f38b57dfefdcffecbbce9a1
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-05-28 14:11:32 +0000

    limit scrolling to the right

--- zilo.c
+++ zilo.c
@@ -395,6 +395,8 @@ void editorRefreshScreen()
 
 void editorMoveCursor(int key)
 {
+    erow *row = (E.numrows <= E.cy) ? NULL : &E.row[E.cy];
+
     switch (key)
     {
     case ARROW_LEFT:
@@ -404,7 +406,10 @@ void editorMoveCursor(int key)
         }
         break;
     case ARROW_RIGHT:
-        E.cx++;
+        if (row && E.cx < row->size)
+        {
+            E.cx++;
+        }
         break;
     case ARROW_UP:
         if (E.cy != 0)
