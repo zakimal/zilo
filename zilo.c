3b16916984e0b419fd967dcbf4b1579eadbaf705
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-05-28 14:35:24 +0000

    tabs and the cursor

--- zilo.c
+++ zilo.c
@@ -47,6 +47,7 @@ typedef struct erow // editor row
 struct editorConfig
 {
     int cx, cy; // cursor position
+    int rx;
     int rowoff; // row offset
     int coloff; // col offset
     int screenrows;
@@ -227,6 +228,21 @@ int getWindowSize(int *rows, int *cols)
 
 /*** row operations ***/
 
+int editorRowCxToRx(erow *row, int cx)
+{
+    int rx = 0;
+    int j;
+    for (j = 0; j < cx; j++)
+    {
+        if (row->chars[j] == '\t')
+        {
+            rx += (ZILO_TAB_STOP - 1) - (rx % ZILO_TAB_STOP);
+        }
+        rx++;
+    }
+    return rx;
+}
+
 void editorUpdateRow(erow *row)
 {
     int tabs = 0;
@@ -333,6 +349,12 @@ void abFree(struct abuf *ab)
 
 void editorScroll()
 {
+    E.rx = 0;
+    if (E.cy < E.numrows)
+    {
+        E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
+    }
+
     if (E.cy < E.rowoff)
     {
         E.rowoff = E.cy;
@@ -342,13 +364,13 @@ void editorScroll()
         E.rowoff = E.cy - E.screenrows + 1;
     }
 
-    if (E.cx < E.coloff)
+    if (E.rx < E.coloff)
     {
-        E.coloff = E.cx;
+        E.coloff = E.rx;
     }
-    if (E.coloff + E.screencols <= E.cx)
+    if (E.coloff + E.screencols <= E.rx)
     {
-        E.coloff = E.cx - E.screencols + 1;
+        E.coloff = E.rx - E.screencols + 1;
     }
 }
 
@@ -421,7 +443,7 @@ void editorRefreshScreen()
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1);
     abAppend(&ab, buf, strlen(buf));
 
     abAppend(&ab, "\x1b[?25h", 6);
@@ -517,6 +539,7 @@ void initEditor()
 {
     E.cx = 0;
     E.cy = 0;
+    E.rx = 0;
     E.rowoff = 0;
     E.coloff = 0;
     E.numrows = 0;
