8e5daaca32c44ca6ab2925e837a242904fec09ab
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-05-29 09:40:04 +0000

    insert ordinary characters

--- zilo.c
+++ zilo.c
@@ -296,6 +296,29 @@ void editorAppendRow(char *s, size_t len)
     E.numrows++;
 }
 
+void editorRowInsertChar(erow *row, int at, int c)
+{
+    if (at < 0 || row->size < at)
+        at = row->size;
+    row->chars = realloc(row->chars, row->size + 2);
+    memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
+    row->size++;
+    row->chars[at] = c;
+    editorUpdateRow(row);
+}
+
+/*** editor operations ***/
+
+void editorInsertChar(int c)
+{
+    if (E.cy == E.numrows)
+    {
+        editorAppendRow("", 0);
+    }
+    editorRowInsertChar(&E.row[E.cy], E.cx, c); // insert a character at the current cursor position
+    E.cx++;
+}
+
 /*** file i/o ***/
 
 void editorOpen(char *filename)
@@ -602,6 +625,10 @@ void editorProcessKeypress()
     case ARROW_LEFT:
     case ARROW_RIGHT:
         editorMoveCursor(c);
+        break;
+
+    default:
+        editorInsertChar(c);
         break;
     }
 }
