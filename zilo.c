6a08bcbb8e0835b7307b38110821450cf143a3e0
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-05-29 09:45:44 +0000

    prevent inserting special characters

--- zilo.c
+++ zilo.c
@@ -25,6 +25,7 @@
 
 enum editorKey
 {
+    BACKSPACE = 127,
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
@@ -585,6 +586,10 @@ void editorProcessKeypress()
 
     switch (c)
     {
+    case '\r':
+        // TODO: implement
+        break;
+
     case CTRL_KEY('q'):
         write(STDOUT_FILENO, "\x1b[2J", 4);
         write(STDOUT_FILENO, "\x1b[H", 3);
@@ -598,6 +603,12 @@ void editorProcessKeypress()
     case END_KEY:
         if (E.cy < E.numrows)
             E.cx = E.row[E.cy].size;
+        break;
+
+    case BACKSPACE:
+    case CTRL_KEY('h'):
+    case DEL_KEY:
+        // TODO: implement
         break;
 
     case PAGE_UP:
@@ -625,6 +636,10 @@ void editorProcessKeypress()
     case ARROW_LEFT:
     case ARROW_RIGHT:
         editorMoveCursor(c);
+        break;
+
+    case CTRL_KEY('l'):
+    case '\x1b': // <esc>
         break;
 
     default:
