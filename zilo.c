199c4be18e9dec4e34da63c804566dfc9f4a9b05
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-05-30 08:49:10 +0000

    colorful search result

--- zilo.c
+++ zilo.c
@@ -42,7 +42,8 @@ enum editorKey
 enum editorHighlight
 {
     HL_NORMAL = 0,
-    HL_NUMBER
+    HL_NUMBER,
+    HL_MATCH
 };
 
 /*** data ***/
@@ -270,7 +271,9 @@ int editorSyntaxToColor(int hl)
     switch (hl)
     {
     case HL_NUMBER:
-        return 31;
+        return 31; // red
+    case HL_MATCH:
+        return 34; // blue
     default:
         return 37;
     }
@@ -599,6 +602,8 @@ void editorFindCallback(char *query, int key)
             E.cy = current;
             E.cx = editorRowRxToCx(row, match - row->render);
             E.rowoff = E.numrows;
+
+            memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
             break;
         }
     }
